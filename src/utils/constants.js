6139bd2b5999397c68fdb3748e5159cff9318331
Denise Hung <denisehung87@gmail.com>
2021-06-24 22:15:31 +0200

    refactor: add new constants and remove obsoletes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,50 +1,31 @@
 // Edit profile variables
 export const editButton = document.querySelector(".profile__edit-button");
 export const editForm = document.querySelector(".popup__form-main_type_edit");
-export const popupEditProfile = ".popup_type_edit";
+
+// Profile details variables
+export const nameInput = document.querySelector(".popup__input_type_name");
+export const jobInput = document.querySelector(".popup__input_type_about");
+export const profileName = document.querySelector(".profile__name");
+export const profileJob = document.querySelector(".profile__about");
+export const profileImage = document.querySelector(".profile__image");
+export const profileImageOverlay = document.querySelector(".profile__image-overlay");
+export const avatarForm = document.querySelector(".popup__form-main_avatar");
 
 // Add image and card variables
 export const addImgButton = document.querySelector(".profile__add-button");
 export const addImgForm = document.querySelector(".popup__form-main_img");
 export const cardContainer = document.querySelector(".image-grid");
+
+// Popups
 export const popupAddImage = ".popup_type_add-img";
 export const popupLargeImage = ".popup_type_image";
+export const popupChangeAvatar = ".popup_type_avatar";
+export const popupDeleteImage = ".popup_type_delete";
+export const popupEditProfile = ".popup_type_edit";
 
-// Profile detail variables
-export const nameInput = document.querySelector(".popup__input_type_name");
-export const jobInput = document.querySelector(".popup__input_type_about");
-export const profileName = document.querySelector(".profile__name");
-export const profileJob = document.querySelector(".profile__about");
-
+export const submitButton = document.querySelector(".popup__submit-button");
 export const cardTemplate = "#card-template";
 export const imageGrid = ".image-grid";
-
-// Array with properties for initial cards
-export const initialCards = [{
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg" 
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  }
-];
 
 export const settings = {
   formSelector: ".popup__form-main",
