0c8a1fa236193c1d15f2c2d774fc96711f652c42
Denise Hung <denisehung87@gmail.com>
2021-06-24 22:34:47 +0200

    feat: create new popup extension

---
+++ src/components/PopupWithSubmit.js
@@ -0,0 +1,36 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithSubmit extends Popup {
+    constructor({popupSelector, handleFormSubmit}) {
+        super(popupSelector);
+        this._handleFormSubmit = handleFormSubmit;
+        this._popupElement = document.querySelector(popupSelector);
+        this._confirmDeleteButton = this._popupElement.querySelector(".popup__submit-button_type_delete");
+        this._buttonText = this._popupElement.querySelector(".popup__submit-button").textContent;
+    }
+    
+    // Pass callback function when submitting data to server
+    setSubmitAction (action) {
+        this._handleFormSubmit = action;
+      }
+    
+    // Change button text to 'Saving...' while uploading data (renderLoading is true)
+    renderLoading(isLoading) {
+        if(isLoading) {
+          this._popupElement.querySelector(".popup__submit-button").textContent = "Saving...";
+        } else {
+          this._popupElement.querySelector(".popup__submit-button").textContent = this._buttonText;
+        }
+      }
+
+    setEventListeners() {
+        super.setEventListeners();
+        
+        this._confirmDeleteButton.addEventListener("click", (evt) => {
+            evt.preventDefault();
+            this._handleFormSubmit();
+            this.ArenderLoading(true);
+            this.close();
+        })       
+    }   
+}
